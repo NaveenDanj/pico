221112efeee762b52ef6f5a4a6d8bf0f13c2633d
naveendanj <naveenhettiwaththa@gmail.com>
2023-11-29 09:53:50 +0530

    added latest changes

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+    // import * as functions from 'firebase-functions';
+    // import * as cors from 'cors';
+
+
+
+    // const corsHandler = cors({origin: true});
+            
+    // export const fetchEmails = functions.https.onRequest(async (request, response) => {
+    //     corsHandler(request, response, () => { 
+    //         response.set("Access-Control-Allow-Origin", "*");
+    //         response.set("Access-Control-Allow-Headers", "Content-Type");
+    //         console.log('running!')
+
+    //     });
+    // });
+
